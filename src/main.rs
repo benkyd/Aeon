69d73302251b5feabb6fdfa6bb3322fa03e91307
Ben <benjaminkyd@gmail.com>
2020-01-08 00:00:49 +0000

    Learning Rust sm h

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+use std::io;
+use rand::Rng;
 
 fn main() 
 {
-    println!("Hello, world!");
+    // let mut x = String::new();
+
+    // io::stdin().read_line(&mut x)
+    //            .expect("Failed to read line");
+    
+    let x = rand::thread_rng().gen_range(1, 101);
+
+    println!("{}", x);
 
 }
+ 
